3dd388236b9176274cec98bc69fdfb8670ae03c1
aswinjosereji2024@mca.ajce.in <aswinjosereji2024@mca.ajce.in>
2024-07-29 22:07:23 +0530

    Landing Page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
+});
+
+
+
+
